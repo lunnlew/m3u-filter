41a32d0d0d85d9cc7101fc9775c21da9d71f0129
刘强 <4288315+lunnlew@users.noreply.github.com>
2025-03-29 19:22:53 +0800

    更新

--- backend/routers/site_config.py
+++ backend/routers/site_config.py
@@ -10,5 +10,5 @@ async def get_site_config():
     """获取站点配置信息"""
     return BaseResponse.success({
         "base_url": BASE_URL,
-        "static_url_prefix": RESOURCE_URL_PREFIX
+        "resource_url_prefix": RESOURCE_URL_PREFIX
     })
